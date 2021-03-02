914499c42e3b85edf307bc872e090624148c3114
Jamp-H <jmh824@nau.edu>
2021-03-02 10:44:42 -0700

    Fixing shell script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
-sudo apt-get install libatlas-base-dev
-sudo apt-get install libjasper-dev
-sudo apt-get install libqtgui4
-sudo apt-get install libqt4-test
-sudo apt-get install python3-pyqt5
+sudo apt-get install libatlas-base-dev -y
+sudo apt-get install libjasper-dev -y
+sudo apt-get install libqtgui4 -y
+sudo apt-get install libqt4-test -y
+sudo apt-get install python3-pyqt5 -y
+
+cd ~/
 
 sudo apt-get install wget build-essential checkinstall -y&& \
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y && \
@@ -12,9 +14,14 @@ cd Python-3.7.9 && \
 ./configure --enable-optimizations && \
 sudo make altinstall
 
-sudo apt-get install python3-venv
+sudo rm -rf Python-3.7.9
+sudo rm -rf Python-3.7.9.tgz
+
+sudo apt-get install python3-venv -y
 
 cd ~/
 mkdir env
-python3 -m venv ~/env/PiEnv
+python3.7 -m venv ~/env/PiEnv
 source ~/env/PiEnv/bin/activate
+
+python -m pip install --upgrade pip
