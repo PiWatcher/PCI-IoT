d0f131f4229be724cfb1cc2ce2c4d6a5d98e4acb
Jamp-H <jmh824@nau.edu>
2021-03-04 04:38:35 -0700

    Undid commented out code

--- setup.sh
+++ setup.sh
@@ -10,18 +10,18 @@ sudo apt-get install libqt4-test -y
 sudo apt-get install python3-pyqt5 -y
 sudo apt-get install python3-h5py -y
 
-#sudo apt-get install wget build-essential checkinstall -y && \
-#sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y && \
-#wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz && \
-#tar xzf Python-3.7.9.tgz && \
-#cd Python-3.7.9 && \
-#./configure --enable-optimizations && \
-#sudo make install
+sudo apt-get install wget build-essential checkinstall -y && \
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y && \
+wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz && \
+tar xzf Python-3.7.9.tgz && \
+cd Python-3.7.9 && \
+./configure --enable-optimizations && \
+sudo make install
 
-#cd ..
+cd ..
 
-#sudo rm -rf Python-3.7.9
-#sudo rm -rf Python-3.7.9.tgz
+sudo rm -rf Python-3.7.9
+sudo rm -rf Python-3.7.9.tgz
 
 python3.7 -m pip install --upgrade pip
 
